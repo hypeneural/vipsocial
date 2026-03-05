ca3080477e306d1054aec91bcd2b0e030c6a9faa
Hype Neural <hypeneural@gmail.com>
2026-03-05 14:13:25 -0300

    build(web): sync dist in api/public with gavetas include user

---
+++ apps/api/public/assets/RoleIcon-BIBlqF7h.js
@@ -0,0 +1,1 @@
+import{j as e,aO as t,a0 as n,Q as r,ad as p,ag as c,u as i}from"./index-DX29HXoV.js";import{S as m}from"./smartphone-Rp50Xib5.js";import{S as h}from"./shield-check-IGB0ZEFA.js";const u={ShieldCheck:h,PenSquare:i,Newspaper:c,Smartphone:m,BarChart3:p,Shield:r,Eye:n,UserCheck:t};function l({name:a,className:o}){const s=a&&u[a]||r;return e.jsx(s,{className:o})}export{l as R};
