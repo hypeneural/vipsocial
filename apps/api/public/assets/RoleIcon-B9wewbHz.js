cf71abb6915e3682fd18e6adedca5edc8d214c9c
Hype Neural <hypeneural@gmail.com>
2026-03-09 16:34:32 -0300

    chore: public assets update for external events table migration

---
+++ apps/api/public/assets/RoleIcon-B9wewbHz.js
@@ -0,0 +1,1 @@
+import{j as o,b5 as t,V as n,J as r,ab as p,ae as c,s as i}from"./index-BF8EtvRh.js";import{S as m}from"./smartphone-DJIWjiyn.js";import{S as h}from"./shield-check-CKhraLPz.js";const S={ShieldCheck:h,PenSquare:i,Newspaper:c,Smartphone:m,BarChart3:p,Shield:r,Eye:n,UserCheck:t};function l({name:a,className:s}){const e=a&&S[a]||r;return o.jsx(e,{className:s})}export{l as R};
