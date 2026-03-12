68fd71b3ee1d2921b727ec707bf597f72746c1c5
Hype Neural <hypeneural@gmail.com>
2026-03-12 20:24:52 -0300

    Update built admin assets

---
+++ apps/api/public/assets/RoleIcon-Bh4l1UHA.js
@@ -0,0 +1,1 @@
+import{j as e,bp as t,ap as n,ai as r,aA as p,aB as i,aq as c}from"./index-CtJQMW5x.js";import{S as m}from"./smartphone-mcXEMjYg.js";import{S as h}from"./shield-check-CVib9hY4.js";const S={ShieldCheck:h,PenSquare:c,Newspaper:i,Smartphone:m,BarChart3:p,Shield:r,Eye:n,UserCheck:t};function l({name:a,className:o}){const s=a&&S[a]||r;return e.jsx(s,{className:o})}export{l as R};
