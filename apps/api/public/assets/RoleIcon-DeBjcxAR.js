5278c9df10cdf2bb02ec0010b5255f9f24a9607c
Hype Neural <hypeneural@gmail.com>
2026-03-12 15:41:11 -0300

    Update built frontend assets

---
+++ apps/api/public/assets/RoleIcon-DeBjcxAR.js
@@ -0,0 +1,1 @@
+import{j as e,bi as t,ag as n,a6 as r,as as i,at as p,ah as c}from"./index-oaRLAYp-.js";import{S as h}from"./smartphone-BkRcM_NY.js";import{S as m}from"./shield-check-1x4DC9J9.js";const S={ShieldCheck:m,PenSquare:c,Newspaper:p,Smartphone:h,BarChart3:i,Shield:r,Eye:n,UserCheck:t};function l({name:a,className:s}){const o=a&&S[a]||r;return e.jsx(o,{className:s})}export{l as R};
